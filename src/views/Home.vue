7e8760ca2d9e789e8864545d8d9f1dac63403c87
Vincent Rubinetti <vince.rubinetti@gmail.com>
2020-12-03 12:58:47 -0500

    add hero

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,7 @@
 <template>
   <Header v-bind:big="true" />
   <Main>
-    <Section>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-      est laborum.
-    </Section>
+    <Hero />
     <Section>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
       tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
@@ -38,13 +30,15 @@ import Header from "@/components/Header.vue";
 import Main from "@/components/Main.vue";
 import Section from "@/components/Section.vue";
 import Footer from "@/components/Footer.vue";
+import Hero from "./home/Hero.vue";
 
 export default defineComponent({
   components: {
     Header,
     Main,
     Section,
-    Footer
+    Footer,
+    Hero
   }
 });
 </script>
