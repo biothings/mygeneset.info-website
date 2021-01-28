dd9d013fed36b7e696c36d18862ae2c9cec0dba7
Vincent Rubinetti <vince.rubinetti@gmail.com>
2021-01-28 16:33:48 -0500

    update

--- src/shim-vuex.d.ts
+++ src/shim-vuex.d.ts
@@ -1,6 +1,7 @@
-import { Store } from "@/store";
 // https://stackoverflow.com/questions/64412243/vue-js-3-and-typescript-property-store-does-not-exist-on-type-componentpub
 // https://next.vuex.vuejs.org/guide/typescript-support.html#typing-store-property-in-vue-component
+
+import { Store } from "@/store";
 
 declare module "@vue/runtime-core" {
   interface ComponentCustomProperties {
