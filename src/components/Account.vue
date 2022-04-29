abcb7f9c122a2ff9989e765a604af34f4369b6ab
Vince Rubinetti <vince.rubinetti@gmail.com>
2022-04-29 16:28:16 -0400

    prevent default on logout link click to fix?

--- src/components/Account.vue
+++ src/components/Account.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- log in/out  and avatar in header -->
   <div class="account">
-    <HeaderLink v-if="loggedIn" text="Log Out" @click="logout" />
+    <HeaderLink v-if="loggedIn" text="Log Out" @click.prevent="logout" />
     <Avatar v-if="loggedIn" />
     <HeaderLink v-else to="/login" text="Log In" />
   </div>
