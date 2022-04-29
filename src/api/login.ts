bc0b4c9f16a1f1954d1e7d341a4bfad43a7ea002
Vincent Rubinetti <vince.rubinetti@gmail.com>
2022-04-29 15:08:34 -0400

    fix login/logout buttons (#27)
    - fixes an issue where vue "helps" by shortening absolute link to relative link because it matches the domain (when deployed to mygeneset.info), causing the link to not work

--- src/api/login.ts
+++ src/api/login.ts
@@ -1,5 +1,22 @@
 import { mygenesetRoot, request } from ".";
 
+// login via github or orcid
+export const login = async (method = "") => {
+  try {
+    return await request(`${mygenesetRoot}login/${method}`);
+  } catch (error) {
+    return null;
+  }
+};
+
+// logout
+export const logout = async () => {
+  try {
+    return await request(`${mygenesetRoot}logout}`);
+  } catch (error) {
+    return null;
+  }
+};
 // get user info and determine whether logged in or not
 export const getUser = async () => {
   try {
--- src/views/login/Accounts.vue
+++ src/views/login/Accounts.vue
@@ -5,13 +5,13 @@
         icon="fab fa-github"
         text="With GitHub"
         design="big"
-        to="https://mygeneset.info/login/github"
+        @click="login('github')"
       />
       <Clickable
         icon="fas fa-graduation-cap"
         text="With ORCID"
         design="big"
-        to="https://mygeneset.info/login/orcid"
+        @click="login('orcid')"
       />
     </Center>
   </Section>
@@ -22,12 +22,16 @@ import { defineComponent } from "vue";
 import Section from "@/components/Section.vue";
 import Center from "@/components/Center.vue";
 import Clickable from "@/components/Clickable.vue";
+import { login } from "@/api/login";
 
 export default defineComponent({
   components: {
     Section,
     Center,
     Clickable
+  },
+  methods: {
+    login
   }
 });
 </script>
